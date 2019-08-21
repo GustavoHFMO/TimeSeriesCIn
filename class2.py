8a4f5540333f597f68cd88a469d2aa9f7812aa50
GustavoHFMO <guga.h.f.m.o@gmail.com>
2019-08-21 14:05:48 -0300

    up

--- class2.py
+++ class2.py
@@ -7,8 +7,9 @@ Created on 21 de ago de 2019
 from pandas import Series
 # lib to plot the data
 import matplotlib.pyplot as plt
-# lib to implort the wolt winter model
+# lib to import the stocastic basic models
 from statsmodels.tsa.api import SimpleExpSmoothing, Holt, ExponentialSmoothing
+# lib to import the ar model
 from statsmodels.tsa.ar_model import AR
 
 # to read the time series
@@ -16,34 +17,66 @@ serie = Series.from_csv('airline-passengers.csv', header=0)
 serie.plot()
 
 #################Simple Exponential#########################
+# fit the data
 ses = SimpleExpSmoothing(serie).fit(smoothing_level=0.8, optimized=True)
-#ses.fittedvalues.plot(label='SES Fit')
+
+# plot the data
+ses.fittedvalues.plot(label='SES Fit')
 ses.forecast(25).rename('SES Forecasting').plot()
+
+# to show the plot
+plt.legend()
+plt.show()
 #############################################################
 
 #################Holt and Winter#############################
+# fit the data
 hw = Holt(serie).fit(smoothing_level=0.8, smoothing_slope=0.9)
-#hw.fittedvalues.plot(label='HW Fit')
+
+# plot the data
+hw.fittedvalues.plot(label='HW Fit')
+
+# to show the plot
 hw.forecast(25).rename('HW Forecasting').plot()
+plt.legend()
+plt.show()
 #############################################################
 
+
 #################Exponential Smoothing#######################
-es = ExponentialSmoothing(serie, seasonal_periods=12, trend='add', seasonal='add', damped=True).fit(use_boxcox=True)
-#es.fittedvalues.plot(label='ES Fit')
+# fit the data
+es = ExponentialSmoothing(serie, 
+                          seasonal_periods=12, 
+                          trend='add', 
+                          seasonal='add', 
+                          damped=True).fit(use_boxcox=True)
+
+# plot the data
+es.fittedvalues.plot(label='ES Fit')
 es.forecast(25).rename('ES Forecasting').plot()
+
+# to show the plot
+plt.legend()
+plt.show()
 #############################################################
 
 #################Auto Regressive#######################
+# train AR
 model = AR(serie)
 model_fitted = model.fit()
+
+# parameters used
 print("lags used: ", model_fitted.k_ar)
 print("parameters: ", model_fitted.params)
 
+# plot the prediction
+predictions = model_fitted.predict(start=model_fitted.k_ar, end=len(serie), dynamic=False)
+predictions.plot(label='AR Fit')
 predictions = model_fitted.predict(start=len(serie), end=len(serie)+25, dynamic=False)
 predictions.plot(label='AR Forecasting')
-#############################################################
 
-#to show plot
+#to show
 plt.legend()
 plt.show()
+#############################################################
 
